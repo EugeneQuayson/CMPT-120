9be62b3846fcaf4a2e50f8d79c0f9331aa9f82a7
EugeneQuayson <115434019+EugeneQuayson@users.noreply.github.com>
2022-11-03 12:28:54 -0400

    Update blackjack.py

--- Assignment 4/blackjack.py
+++ Assignment 4/blackjack.py
@@ -1,12 +1,52 @@
-#if you don't know how to play blackjack, tbh not super important but look it up anyway LOL
-#this script is going to require some googling: I want you to practice using your resources with this one. But of course if you get stuck, reach out :)
-'''instructions: randomly generate three values between 1 and 11. in the function bust: add these three numbers together. if they add up to or less than 21, return the sum. If it's over 21, return 0. If it's over 21 BUT there's an 11 as one of the values, return the sum - 10. '''
+import random
+
 
 def bust():
-    
-    
-    
+    numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
+
+    val1 = random.choice(numbers)
+    val2 = random.choice(numbers)
+    val3 = random.choice(numbers)
+
+    total = val1 + val2 + val3
+
+    print("Card One: ", val1)
+    print("Card Two: ", val2)
+    print("Card Three: ", val3)
+
+    if 11 in {val1, val2, val3}:
+        result = total - 10
+        if result > 21:
+            print("Bust!")
+        elif result < 21:
+            print(result)
+        else:
+            print("Blackjack!")
+    else:
+        if total > 21:
+            print("Bust!")
+        elif total < 21:
+            print(total)
+        else:
+            print("Blackjack!")
+
+
 def main():
-    
+    while True:
+        play = input("Would you like to play a game of Blackjack? (Y or N) ")
+        if play == "Y":
+            bust()
+        else:
+            print("Ok, bye!")
+
+    while True:
+        cont = input("Would you like to play again? (Y or N) ")
+
+        if cont == "Y":
+            bust()
+        else:
+            print("Ok, bye!")
+            break
+
 
 main()
